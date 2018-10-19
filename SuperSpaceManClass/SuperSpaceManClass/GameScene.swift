67ee3ca55ac9e41971d68bf46713a8e5c3cbc33a
apple <apple@a2.local>
2018-10-19 13:05:09 +0800

    Chapter 3 Summary
    Learning about :
    Adding physics and collision detection
    
    Sub : Adding physics to game world
    learning about how to add physics body to node and how to set the gravity by modify the world's gravity property
    
    Sub : Adding Forces
    learning about how to apply impulse, how the touch will respond, and linearDamping property
    
    Sub : Adding Collision
    learning about  how to add a node to collide (orbNode)
    
    Sub : Adding BitMasks
    learning about what is CollisionBitMask, categoryBitMask, and contactTestBitMask
    
    Sub : Removing orb when receive contact message
    learning about what is the purpose of SKPhysicsContact properties and how to implement them

--- SuperSpaceManClass/SuperSpaceManClass/GameScene.swift
+++ SuperSpaceManClass/SuperSpaceManClass/GameScene.swift
@@ -12,6 +12,11 @@ class GameScene: SKScene {
     
     let backgroundNode = SKSpriteNode(imageNamed: "Background") //SKSPriteNode is a node that used to draw textured sprite
     let playerNode = SKSpriteNode(imageNamed: "Player") //imageNamed means the node will use the image called Player
+    let orbNode = SKSpriteNode(imageNamed: "PowerUp") //adding a node for PowerUp image
+    
+    let CollisionCategoryPlayer : UInt32 = 0x1 << 1
+    //defines the category to which a physics body belongs to (example: this collision category belongs to Player)
+    let CollisionCategoryPowerUpOrbs : UInt32 = 0x1 << 2
     
     
     required init?(coder aDecoder: NSCoder) {
@@ -21,16 +26,57 @@ class GameScene: SKScene {
     override init(size: CGSize) {
         super.init(size: size)
         
+        physicsWorld.contactDelegate = self
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.1) // modified the world's gravity
+        
+        
+        backgroundColor = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        isUserInteractionEnabled = true //turns on user interaction in the scene
+        
+        //add background
         backgroundNode.size.width = frame.size.width
         backgroundNode.anchorPoint = CGPoint(x: 0.5, y: 0.0)
         backgroundNode.position = CGPoint(x: size.width / 2.0, y: 0.0)
         addChild(backgroundNode)
         
+        //add player
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: playerNode.size.width / 2)
+        playerNode.physicsBody?.isDynamic = true
+        
         playerNode.position = CGPoint(x: size.width / 2.0, y: 80.0)
+        playerNode.physicsBody?.linearDamping = 1.0 //the player will fall but slowly due the small value on damping
+        playerNode.physicsBody?.allowsRotation = false //the player will not spin but it will bounce off
+        playerNode.physicsBody?.categoryBitMask = CollisionCategoryPlayer
+        playerNode.physicsBody?.contactTestBitMask = CollisionCategoryPowerUpOrbs //notify if this physics body contact with another physics body belongs to PowerUpOrbs category
+        playerNode.physicsBody?.collisionBitMask = 0 //tells SpriteKit not to handle collisions
         addChild(playerNode)
+        
+        //add orbNode
+        orbNode.position = CGPoint(x: 150.0, y: size.height - 25)
+        orbNode.physicsBody = SKPhysicsBody(circleOfRadius: orbNode.size.width / 2)
+        orbNode.physicsBody?.isDynamic = false
+        orbNode.physicsBody?.categoryBitMask = CollisionCategoryPowerUpOrbs
+        orbNode.physicsBody?.collisionBitMask = 0
+        orbNode.name = "POWER_UP_ORB"
+        addChild(orbNode)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        playerNode.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 40.0))
+    }
+    
     
 }
 
-//creating GameScene background for the first time
+extension GameScene: SKPhysicsContactDelegate { //extension of GameScene
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        let nodeB = contact.bodyB.node
+        
+        if nodeB?.name == "POWER_UP_ORB" {
+            nodeB?.removeFromParent()
+        }
+    }
+}
 
+//creating GameScene background for the first time
